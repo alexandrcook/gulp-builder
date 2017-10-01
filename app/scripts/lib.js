bf8e28c605ef0d3026d8e30aac4d17b1593c62c5
alexcook <alexcook@alexcooks-iMac.local>
2017-10-01 13:56:13 +0300

    start gulp-builder project

---
+++ app/scripts/lib.js
@@ -0,0 +1,18 @@
+/* NPM files requiring */
+
+//=require /jquery/dist/jquery.min.js
+//=require /lightslider/dist/js/lightslider.min.js
+
+/* Bower files requiring */
+
+
+/* Part files requiring */
+//=require parts/partfile.js
+
+console.log('its working 2');
+
+console.log('3');
+
+function test() {
+    //do something
+}
---
+++ app/scripts/parts/partfile.js
@@ -0,0 +1,1 @@
+console.log('its part file');
